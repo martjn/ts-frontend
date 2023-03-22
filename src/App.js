30b2a95fe12d776a000761256d49a5592834e635
martjn <mingdev77@gmail.com>
2023-03-22 14:37:49 +0200

    pakiautomaadid

--- src/App.js
+++ src/App.js
@@ -1,68 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [tooted, setTooted] = useState([]);
-    const idRef = useRef();
-    const nameRef = useRef();
-    const priceRef = useRef();
-    const isActiveRef = useRef();
+    const [pakiautomaadid, setPakiautomaadid] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3000/tooted")
+        fetch("http://localhost:3000/parcelmachines")
             .then(res => res.json())
-            .then(json => setTooted(json));
+            .then(json => setPakiautomaadid(json));
+            console.log(pakiautomaadid)
     }, []);
 
-    function kustuta(index) {                             ////////////////////////
-        fetch("http://localhost:3000/kustuta-toode/" + index, {"method": "DELETE"})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    function lisa() {
-        const uusToode = {
-            "id": idRef.current.value,
-            "name": nameRef.current.value,
-            "price": priceRef.current.value,
-            "isActive": isActiveRef.current.value
-        }
-        fetch("http://localhost:3000/lisa-toode", {"method": "POST", "body": JSON.stringify(uusToode)})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    function dollariteks() {
-        const kurss = 1.1;                                      ////////////////////////
-        fetch("http://localhost:3000/hind-dollaritesse/" + kurss, {"method": "PATCH"})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-
     return (
-      <div className="App">
-        <label>ID</label> <br />
-        <input ref={idRef} type="number" /> <br />
-        <label>Nimi</label> <br />
-        <input ref={nameRef} type="text" /> <br />
-        <label>Hind</label> <br />
-        <input ref={priceRef} type="number" /> <br />
-        <label>Aktiivne</label> <br />
-        <input ref={isActiveRef} type="checkbox" /> <br />
-        <button onClick={() => lisa()}>Lisa</button>
-          {console.log(tooted)}
-        {tooted.map(toode =>
-            <div>
-              <div>{toode.id}</div>
-              <div>{toode.name}</div>
-              <div>{toode.price}</div>
-              <button onClick={() => kustuta(toode.id)}>Kustuta</button>
-            </div>)}
-
-          <button onClick={() => dollariteks()}>Muuda dollariteks</button>
-      </div>
-  );
+        <div className="App">
+            <select>
+                {pakiautomaadid.map(automaat =>
+                    <option>
+                        {automaat.NAME}
+                    </option>)}
+            </select>
+        </div>
+    );
 }
 
 export default App;
